2d3f1f3bb000ed63affd138f041c5a17a706835b
mu-narmandakh-wq <mu-narmandakh@gl-navi.co.jp>
2026-05-07 11:35:41 +0900

    Update newgrad-register.js

--- newgrad-register.js
+++ newgrad-register.js
@@ -1,1 +1,17 @@
-
+EntryForm.create({
+  endpoint: 'https://YOUR-PIPEDREAM/newgrad-register',
+  successUrl: 'https://recruit.gl-navi.co.jp/register/successful',
+  formType: 'newgrad-register',
+  layout: [
+    ['lastName', 'firstName'],
+    ['email', 'emailConfirmation'],
+    ['phone', 'graduationYear']
+  ],
+  marketo: { fields: {
+    LastName: 'lastName', FirstName: 'firstName', Email: 'email', Phone: 'phone',
+    graduation: 'graduationYear',
+    praivacyPolicy: function(d){ return d.privacyPolicy ? 'yes' : 'no'; },
+    recordtype: function(){ return '応募者_新卒'; }
+  }},
+  emailFallback: { subject: function(){ return '新卒登録 (フォームエラー)'; } }
+});
