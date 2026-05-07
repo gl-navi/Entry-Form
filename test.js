2bd3621fb2afbd0cee0368bf9b75b6a38d61ecbe
mu-narmandakh-wq <mu-narmandakh@gl-navi.co.jp>
2026-05-07 11:58:38 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,22 @@
 EntryForm.create({
-  endpoint: 'https://eo82mc181i1aie2.m.pipedream.net',
-  successUrl: 'https://recruit.gl-navi.co.jp/register/successful',
-  formType: 'newgrad-register',
+  endpoint: 'https://eo2yx6wet8n3rr7.m.pipedream.net',
+  successUrl: 'https://recruit.gl-navi.co.jp/apply/successful',
+  formType: 'midcareer-apply',
   layout: [
     ['lastName', 'firstName'],
     ['email', 'emailConfirmation'],
-    ['phone', 'graduationYear']
+    ['phone', 'resume'],
+    ['cv']
   ],
+  queryParams: {
+    desiredOccupation: { queryParam: 'occupation', transform: 'lowercase', mapping: EntryForm.OCCUPATIONS },
+    recordType: { queryParam: 'rt', pathSegment: true, transform: 'lowercase', mapping: EntryForm.RECORD_TYPES, default: '中途コンサルレコードタイプ' },
+    sourcePlatform: { queryParam: 'source', transform: 'lowercase' }
+  },
   marketo: { fields: {
     LastName: 'lastName', FirstName: 'firstName', Email: 'email', Phone: 'phone',
-    graduation: 'graduationYear',
     praivacyPolicy: function(d){ return d.privacyPolicy ? 'yes' : 'no'; },
-    recordtype: function(){ return '応募者_新卒'; }
+    recordtype: function(){ return '応募者_中途'; }
   }},
-  emailFallback: { subject: function(){ return '新卒登録 (フォームエラー)'; } }
+  emailFallback: { subject: function(){ return '中途採用応募 (フォームエラー)'; } }
 });
